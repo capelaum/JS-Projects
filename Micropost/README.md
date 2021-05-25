5f06417cc01b427d88969d03e4a0085b36705500
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-05-25 03:39:08 -0300

    Add MicroPosts Project initial template
    Started to get posts from the JSON server Fake API

---
+++ Micropost/README.md
@@ -0,0 +1,48 @@
+# Micropost
+
+Simple application for creating and managing small posts using mainly Javascript
+
+## Babel Webpack Starter
+
+A starter pack to build JavaScript applications using standards from ES2015, ES2016 & ES2017. It uses webpack, Babel and webpack-dev-server to compile and serve. It is fully compatible with Async/Await as it uses the Babel polyfill.
+
+## Usage
+
+### Installation
+
+Install the dependencies
+
+```sh
+$ npm install
+```
+
+### Serve
+To serve in the browser  - Runs webpack-dev-server
+
+```sh
+$ npm start
+```
+
+### Build
+Compile and build
+
+```sh
+$ npm run build
+```
+
+## More Info
+
+### Tech
+
+- [Webpack](https://webpack.js.org/)
+- [Babel](https://babeljs.io/)
+- [Bootswatch](https://bootswatch.com/)
+- [JSON-Server](https://www.npmjs.com/package/json-server)
+
+### Credits
+
+Made in this [Udemy JavaScript Course](https://www.udemy.com/course/modern-javascript-from-the-beginning) from Brad Traversy - [Traversy Media](http://www.traversymedia.com)
+
+### License
+
+This project is licensed under the MIT License
---
+++ Micropost/src/http.js
@@ -0,0 +1,53 @@
+/**
+ * EasyHTTP Library
+ * Library for making HTTP requests
+ *
+ * @version 3.0.0
+ * @author  Luis Capelletto
+ * @license MIT
+ *
+ **/
+
+export const EasyHTTP = {
+  // Make an HTTP GET Request
+  get: async url => await fetch(url).then(response => response.json()),
+
+  // Make an HTTP POST Request
+  async post(url, data) {
+    const responseData = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(response => response.json());
+
+    return responseData;
+  },
+
+  // Make an HTTP PUT Request
+  async put(url, data) {
+    const responseData = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(response => response.json());
+    
+    return responseData;
+  },
+
+  // Make an HTTP DELETE Request
+  async delete(url) {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const resData = await "Resource Deleted..";
+    return resData;
+  },
+};
